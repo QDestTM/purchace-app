5c2f137c88877e4a9a1fab5b8e9bf7bbbf29ea6e
QDestTM <dimag4681@gmail.com>
2025-03-31 23:44:45 +0300

    Initial commit of project.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.qdesttm.sqltest"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "28.0.13004108"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.qdesttm.sqltest"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/qdesttm/sqltest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.qdesttm.sqltest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
