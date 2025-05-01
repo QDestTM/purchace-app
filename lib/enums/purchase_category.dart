3773234ba643d1837a5a3ae2c34896c81b4e2ce9
QDestTM <dimag4681@gmail.com>
2025-05-01 08:47:13 +0300

    PurchaseCategory enumeration was created.

---
+++ lib/enums/purchase_category.dart
@@ -0,0 +1,17 @@
+enum PurchaseCategory
+{
+	none,
+	food,
+	electronics,
+	clothing,
+	cosmetics,
+	homeAppliances,
+	books,
+	toys,
+	furniture,
+	autoAccesories,
+	sports,
+	medicine,
+	pets,
+	stationery
+}
