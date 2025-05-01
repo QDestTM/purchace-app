91e4599d7985b7d2883fa14ef6b281f9f0d39285
QDestTM <dimag4681@gmail.com>
2025-05-01 08:45:44 +0300

    Added locales and and changed bottom bar color in main module.

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,64 @@
+
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:sql_test/pages/home_page.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+
 
 void main()
 {
-	runApp(const MyApp());
+	final scheme = ColorScheme.fromSeed(seedColor: Colors.teal);
+
+	// Setting color of bottom bottom nav-bar for mobile devices
+	SystemChrome.setSystemUIOverlayStyle(
+		SystemUiOverlayStyle(
+			systemNavigationBarColor: scheme.secondaryFixed
+		),
+	);
+
+	// Initializing app widget
+	final Widget app = SQLTest(scheme: scheme);
+
+	// Starting app
+	runApp(app);
 }
 
 
-class MyApp extends StatelessWidget
+class SQLTest extends StatelessWidget
 {
-	const MyApp({super.key});
+	// ^ ----------------------------------------------------------------------------------------------------<
+
+	// Members
+	final ColorScheme scheme;
+
+	// Constructors
+	const SQLTest({
+		super.key, required this.scheme
+	});
+
+	// # ----------------------------------------------------------------------------------------------------<
 
 	@override
 	Widget build(BuildContext context)
 	{
+		final theme = ThemeData(colorScheme: scheme);
+
+		// Building tree of widgets
 		return MaterialApp(
-			title: 'SQL Test',
-			theme: ThemeData(
-				colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-			),
-			home: const MyHomePage(title: 'Flutter Demo Home Page'),
+			title: 'SQL Test', theme: theme,
+			supportedLocales: [
+				Locale('uk', 'UA')
+			],
+
+			localizationsDelegates: [
+				...GlobalMaterialLocalizations.delegates,
+				GlobalWidgetsLocalizations.delegate
+			],
+
+			home: const HomePage(),
 			debugShowCheckedModeBanner: false,
 		);
 	}
-}
 
-
-class MyHomePage extends StatefulWidget
-{
-	const MyHomePage({super.key, required this.title});
-
-	final String title;
-
-	@override
-	State<MyHomePage> createState() => _MyHomePageState();
-}
-
-
-class _MyHomePageState extends State<MyHomePage>
-{
-	int _counter = 0;
-
-	void _incrementCounter()
-	{
-		setState(() {
-			_counter += 1;
-		});
-	}
-
-	@override
-	Widget build(BuildContext context)
-	{
-		return Scaffold(
-			appBar: AppBar(
-				backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-				title: Text(widget.title),
-			),
-			body: Center(
-				child: Column(
-					mainAxisAlignment: MainAxisAlignment.center,
-					children: <Widget>
-					[
-						const Text('You have pushed the button this many times:'),
-							Text('$_counter',
-							style: Theme.of(context).textTheme.headlineMedium,
-						),
-					],
-				),
-			),
-			
-			floatingActionButton: FloatingActionButton(
-				onPressed: _incrementCounter,
-				tooltip: 'Increment',
-				child: const Icon(Icons.add),
-			),
-		);
-	}
+	// ------------------------------------------------------------------------------------------------------<
 }
